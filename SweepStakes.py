beba4e112127cc00a25fbd0ab33457acfda45452
DAVID paredes <augustspies22@gmail.com>
2021-04-27 08:45:54 -0700

    Adding seperate Linked List files

--- SweepStakes.py
+++ SweepStakes.py
@@ -4,31 +4,31 @@ import random
 class SweepStakes:
 
     def pick_winner(self):
-        pick_one = random.choice(list(Contestants().contestantList.values()))
+        pick_one = random.choice(Contestants().contestantList)
         print(pick_one)
 
 
 class Contestants:
 
-    contestantList = {
-        'contestant_one': {
+    contestantList = [
+        {
              'first_name': 'David',
              'last_name': 'Paredes'
-         },
-        'contestant_two': {
+        },
+        {
             'first_name': 'Tori',
             'last_name': 'Paredes'
         },
-        'contestant_three': {
+        {
             'first_name': 'Tina',
             'last_name': 'Paredes'
         },
-        'contestant_four': {
+        {
             'first_name': 'Kim',
             'last_name': 'Paredes'
         },
-        'contestant_five': {
+        {
             'first_name': 'Carlos',
             'last_name': 'Paredes'
         }
-    }
+    ]
